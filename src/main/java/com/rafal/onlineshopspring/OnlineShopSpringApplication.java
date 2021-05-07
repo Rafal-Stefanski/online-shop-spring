88e9942e101bdabd75fba585964b1c05674045f3
Rafał Stefański <rafal-stefanski@wp.pl>
2021-05-07 15:58:19 +0200

    Swagger 2.9.2 added (M)

--- src/main/java/com/rafal/onlineshopspring/OnlineShopSpringApplication.java
+++ src/main/java/com/rafal/onlineshopspring/OnlineShopSpringApplication.java
@@ -6,11 +6,26 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class OnlineShopSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineShopSpringApplication.class, args);
+    }
+
+    @Bean
+    public Docket getDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
