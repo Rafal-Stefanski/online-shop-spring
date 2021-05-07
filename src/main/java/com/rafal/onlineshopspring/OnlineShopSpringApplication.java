09e68f41e70b88ba74648ebd39fd405072d8747b
Rafał Stefański <rafal-stefanski@wp.pl>
2021-05-07 18:14:36 +0200

    cleanup (M)

--- src/main/java/com/rafal/onlineshopspring/OnlineShopSpringApplication.java
+++ src/main/java/com/rafal/onlineshopspring/OnlineShopSpringApplication.java
@@ -2,10 +2,7 @@ package com.rafal.onlineshopspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.EventListener;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
