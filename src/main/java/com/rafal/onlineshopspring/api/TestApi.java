037ea138db1677cd3dbb78a817b53195526fb785
Rafał Stefański <rafal-stefanski@wp.pl>
2021-08-11 22:40:04 +0200

    testing RestApi in TestApi.java

--- src/main/java/com/rafal/onlineshopspring/api/TestApi.java
+++ src/main/java/com/rafal/onlineshopspring/api/TestApi.java
@@ -2,16 +2,25 @@ package com.rafal.onlineshopspring.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test")
 public class TestApi {
 
     @GetMapping
+//    @RequestMapping(value = "/testWithRequest", method = RequestMethod.GET)
     public ResponseEntity<String> get() {
         return new ResponseEntity("Hello World!", HttpStatus.BAD_REQUEST);
+    }
+
+    @DeleteMapping
+    public String getThis() {
+        return "test DeleteMapping";
+    }
+
+    @PostMapping
+    public String postMapping() {
+        return "Hello test with POST";
     }
 }
